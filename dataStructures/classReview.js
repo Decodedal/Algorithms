15db391010bd183b78f2e98dd66f08c6e9ca5a4b
Dallas Palumbo <palumbodallas@gmail.com>
2023-02-21 23:51:40 -0500

    stay classy

---
+++ dataStructures/classReview.js
@@ -0,0 +1,10 @@
+class Student{
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName
+    }
+}
+
+const Dallas = new Student("Dallas", "Palumbo")
+
+console.log(Dallas.firstName)
