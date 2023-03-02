a5cc2a0c44b31e50ef961da353a955e25e3be874
Dallas Palumbo <palumbodallas@gmail.com>
2023-03-02 13:47:57 -0500

    ques done

--- dataStructures/queue.js
+++ dataStructures/queue.js
@@ -2,14 +2,14 @@
 
 //Que with an array 
 
-let q = []
+// let q = []
 
-q.unshift("1")
-q.unshift("2")
-q.unshift("3")
+// q.unshift("1")
+// q.unshift("2")
+// q.unshift("3")
 
-q.pop()
-console.log(q)
+// q.pop()
+// console.log(q)
 
 //class based Q 
 class Node{
@@ -23,42 +23,79 @@ class Que{
     constructor(){
         this.first = null;
         this.last = null;
-        this.length = 0; 
+        this.size = 0; 
     }
 
 
-    enQue(value){
-        const newNode = new Node(value)
-        if(this.length = 0){
-            this.first = newNode;
-            this.last = newNode;
-        }else{
-            newNode.next = this.first;
-            this.first = newNode;
+    print(){
+        let currentNode = this.first;
+        for(let i = 0; i < this.size; i++){
+            console.log(currentNode.value);
+            currentNode = currentNode.next
         }
-        this.length++;
-        return this;
+    }
+
+    enQue(value){
+        const node = new Node(value)
+        if(!this.first){
+            this.first = node;
+            this.last = node;
+            this.size++
+        }else{
+            this.last.next = node;
+            this.last = node;
+            this.size++
+        }
+        return this
     }
 
     deQue(){
-        if(this.length <= 1){
+        if(this.size <= 1){
             this.first = null;
             this.last = null;
-            this.length = 0;
+            this.size = 0;
+        }else{
+            this.first = this.first.next;
+            this.size-- ;
         }
-        const current = this.first;
-        for(let i = 0; i < this.length -1; i++){
-            console.log(current)
-            current = current.next;
-            console.log(current)
-        }
-        const poped = this.last;
-        // current.next = null;
-        this.last = current;
-        this.length--;
-        if(this.length === 1) this.first = current;
-        return poped
+        return this;
     }
+
+
+//my mistake was that to remove from this linked list at the end you have to go all the way through the list to the end adding to the end and removing from the beginning keeps time complexity to O(1)
+
+    // enQue(value){
+    //     const newNode = new Node(value)
+    //     if(this.length = 0){
+    //         this.first = newNode;
+    //         this.last = newNode;
+    //     }else{
+    //         newNode.next = this.first;
+    //         this.first = newNode;
+    //     }
+    //     this.length++;
+    //     return this;
+    // }
+
+    // deQue(){
+    //     if(this.length <= 1){
+    //         this.first = null;
+    //         this.last = null;
+    //         this.length = 0;
+    //     }
+    //     const current = this.first;
+    //     for(let i = 0; i < this.length -1; i++){
+    //         console.log(current)
+    //         current = current.next;
+    //         console.log(current)
+    //     }
+    //     const poped = this.last;
+    //     // current.next = null;
+    //     this.last = current;
+    //     this.length--;
+    //     if(this.length === 1) this.first = current;
+    //     return poped
+    // }
 }
 
 const que = new Que;
@@ -70,5 +107,6 @@ que.deQue()
 que.deQue()
 
 console.log(que)
+que.print()
 
 //needs fixing
